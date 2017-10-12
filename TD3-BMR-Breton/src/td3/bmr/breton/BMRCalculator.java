a6142a6b337bfa190337a971d96411ecf0856a9b
Gabriel Breton <Breton@43397>
2017-10-13 00:48:38 +0200

    Add labels and fields for datas gridpane. Create LifeStyle enum.

--- TD3-BMR-Breton/src/td3/bmr/breton/BMRCalculator.java
+++ TD3-BMR-Breton/src/td3/bmr/breton/BMRCalculator.java
@@ -1,6 +1,11 @@
 package td3.bmr.breton;
 
+import javafx.scene.control.Label;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -22,16 +27,56 @@ public class BMRCalculator extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Calcul du BMR");
-        VBox root = new VBox();
+        primaryStage.setTitle("Calcul du BMR"); // to center.
+        primaryStage.setMinWidth(400);
+        primaryStage.setMinHeight(300);
         
+        //create layouts
+        VBox root = new VBox(4);        
         HBox infosZone = new HBox();
-        GridPane userInfos = new GridPane();
-        GridPane userBMR = new GridPane();
+        GridPane datas = new GridPane();
+        GridPane results = new GridPane();
         
+    //create components for datas gridpane
+        //labels
+        Label datasTitle = new Label("Données");
+        Label lblSize = new Label("Taille (cm)");
+        Label lblWeight = new Label("Poids (kg)");
+        Label lblAge = new Label("Age (années)");
+        Label lblSex = new Label("Sexe");
+        Label lblLifeStyle = new Label("Style de vie");
+        
+        //fields
+        TextField tfdSize = new TextField("Taille en cm");
+        TextField tfdWeight = new TextField("Poids en kg");
+        TextField tfdAge = new TextField("Age en années");
+        
+        //for the radio buttons
+        final ToggleGroup groupChoiceSex = new ToggleGroup();
+        
+        RadioButton rbWomen = new RadioButton("Femme");
+        rbWomen.setToggleGroup(groupChoiceSex);
+        rbWomen.setSelected(true);
+        
+        RadioButton rbMen = new RadioButton("Homme");
+        rbMen.setToggleGroup(groupChoiceSex);
+        
+        
+        //create components for results gridpane
+        Label resultsTitle = new Label("Résultats");
+        
+        //add components
+        datas.add(datasTitle, 0, 0);
+        results.add(resultsTitle, 0, 0);
+
+        //add layouts
         root.getChildren().add(infosZone);
-        infosZone.getChildren().addAll(userInfos, userBMR);
+        infosZone.getChildren().addAll(datas, results);
         
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);        
         primaryStage.show();
     }
+    
+   
 }
---
+++ TD3-BMR-Breton/src/td3/bmr/breton/LifeStyle.java
@@ -0,0 +1,35 @@
+package td3.bmr.breton;
+
+/**
+ * This enumeration contains all the life style values.
+ * 
+ * @author Gabriel Breton - 43397
+ */
+public enum LifeStyle {
+    
+    SEDENTARY ("Sédentaire"),
+    LOW_ACTIV ("Peu actif"),
+    ACTIV ("Actif"),
+    VERY_ACTIV ("Fort actif"),
+    EXTREM_ACTIV ("Extrêmement actif");
+    
+    private final String lifeStyle;
+    
+    /**
+     * Creates a lifestyle.
+     * 
+     * @param lifeStyle 
+     */
+    LifeStyle (String lifeStyle) {
+        this.lifeStyle = lifeStyle;
+    }
+    
+    /**
+     * Gives the lifestyle.
+     * 
+     * @return the lifestyle.
+     */
+    public String getLifeStyle() {
+        return lifeStyle;
+    }
+}
