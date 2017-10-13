60de71900baa77d25f7a610d281ec96b4ee27ea3
Gabriel Breton <Breton@43397>
2017-10-13 09:20:54 +0200

    Add calcul button + radio button.

--- TD3-BMR-Breton/src/td3/bmr/breton/BMRCalculator.java
+++ TD3-BMR-Breton/src/td3/bmr/breton/BMRCalculator.java
@@ -2,11 +2,9 @@ package td3.bmr.breton;
 
 import javafx.scene.control.Label;
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
@@ -41,46 +39,52 @@ public class BMRCalculator extends Application {
         Border border =
                new Border(new BorderStroke(Color.GRAY, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(3)));
         
-        primaryStage.setTitle("Calcul du BMR"); // to center.
+        primaryStage.setTitle("Calcul du BMR"); 
         primaryStage.setMinWidth(400);
         primaryStage.setMinHeight(300);
         
         //create layouts
-        VBox root = new VBox(4);      
+        VBox root = new VBox();      
         HBox infosZone = new HBox();
-
+        infosZone.setPadding(new Insets(15));
+        HBox sexChoice = new HBox(20);
+        
         GridPane datas = new GridPane();
-        datas.setBorder(border);
-        datas.setPadding(new Insets(20));
+        datas.setPadding(new Insets(5));
         datas.setHgap(10);
         datas.setVgap(15);
         
         GridPane results = new GridPane();
+        results.setPadding(new Insets(5));
+        results.setHgap(10);
+        results.setVgap(15);
         
-        
-    //create components for datas gridpane
-        //labels
-        
+        //create components for datas gridpane
         Label datasTitle = new Label("Données");
+        datasTitle.setUnderline(true);
         datas.add(datasTitle, 0, 0);
-        
         
         Label lblSize = new Label("Taille (cm)");
         datas.add(lblSize, 0, 1);
-        TextField tfdSize = new TextField("Taille en cm");
+        TextField tfdSize = new TextField();
+        tfdSize.setPromptText("Taille en cm");
         datas.add(tfdSize, 1, 1);
         
         Label lblWeight = new Label("Poids (kg)");
         datas.add(lblWeight, 0, 2);
-        TextField tfdWeight = new TextField("Poids en kg");
+        TextField tfdWeight = new TextField();
+        tfdWeight.setPromptText("Poids en kg");
         datas.add(tfdWeight, 1, 2);
         
         Label lblAge = new Label("Age (années)");
         datas.add(lblAge, 0, 3);
-        TextField tfdAge = new TextField("Age en années");
+        TextField tfdAge = new TextField();
+        tfdAge.setPromptText("Age en années");
         datas.add(tfdAge, 1, 3);
         
-        
+        Label lblSex = new Label("Sexe");
+        datas.add(lblSex, 0, 4);
+      
         final ToggleGroup groupChoiceSex = new ToggleGroup();
 
         RadioButton rbWomen = new RadioButton("Femme");
@@ -89,30 +93,44 @@ public class BMRCalculator extends Application {
         
         RadioButton rbMen = new RadioButton("Homme");
         rbMen.setToggleGroup(groupChoiceSex);
+        datas.add(sexChoice, 1, 4);
+        sexChoice.getChildren().addAll(rbWomen, rbMen);
         
-        Label lblSex = new Label("Sexe");
-        datas.add(lblSex, 0, 4);
-        //datas.add(groupChoiceSex, 4, 4);
         
         Label lblLifeStyle = new Label("Style de vie");
         datas.add(lblLifeStyle, 0, 5);
 
         ChoiceBox cbLifeStyle = new ChoiceBox();
         cbLifeStyle.getItems().addAll(LifeStyle.values());
+        cbLifeStyle.getSelectionModel().selectFirst();;
         datas.add(cbLifeStyle, 1, 5);        
-        
-        //choicebox datas
-
-
         
         //create components for results gridpane
         Label resultsTitle = new Label("Résultats");
+        resultsTitle.setUnderline(true);
         results.add(resultsTitle, 0, 0);
-
-
+        
+        Label lblBMR = new Label("BMR");
+        results.add(lblBMR, 0, 1);
+        
+        TextField tfdBMR = new TextField();
+        tfdBMR.setPromptText("Résultats du BMR");
+        results.add(tfdBMR, 1, 1);
+        
+        Label lblCalories = new Label("Calories");
+        results.add(lblCalories, 0, 2);
+        
+        TextField tfdCalories = new TextField();
+        tfdCalories.setPromptText("Dépenses en calories");
+        results.add(tfdCalories, 1, 2);
+        
+        Button btnCalcul = new Button("Calcul du BMR");
+        btnCalcul.setMaxWidth(Double.MAX_VALUE);
+        
         //add layouts
-        root.getChildren().add(infosZone);
         infosZone.getChildren().addAll(datas, results);
+        
+        root.getChildren().addAll(infosZone, btnCalcul);
         
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);        
