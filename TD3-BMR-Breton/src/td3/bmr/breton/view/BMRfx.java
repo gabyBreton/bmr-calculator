0f210bbce951e6e4eaca4b8189f981c652e5fea2
Gabriel Breton <Breton@43397>
2017-11-17 13:52:38 +0100

    Try to fix the bug of the non updating charts

--- TD3-BMR-Breton/src/td3/bmr/breton/view/BMRfx.java
+++ TD3-BMR-Breton/src/td3/bmr/breton/view/BMRfx.java
@@ -4,15 +4,18 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.Tab;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import td3.bmr.breton.model.BMRCalculator;
+import javafx.scene.control.TabPane;
 
 //TO-DO: REMOVE ALERTS AND SAY INVALID FIELDS !
 /**
@@ -25,8 +28,15 @@ public class BMRfx extends Application {
     private BMRCalculator person;
     private DatasPane datas;
     private ResultsPane results;
-    private ChartTabPane chartTabs;
-    
+    //private ChartTabPane chartTabs;
+    private TabPane chartTabPane;
+    private Tab tabWeightBMR;
+    private Tab tabWeightCal;
+    private Tab tabHeightBMR;
+    private LineCharts chartHeightBMR;
+    private LineCharts chartWeightBMR;
+    private LineCharts chartWeightCal;
+
     /**
      * Launch the window of the BMR Calculator.
      *
@@ -52,10 +62,11 @@ public class BMRfx extends Application {
         datas = new DatasPane(); //datas-zone of user
         results = new ResultsPane(); //results-zone of user
         person = new BMRCalculator();
-        chartTabs = new ChartTabPane();
-        
-        person.addAllObserver(results, chartTabs.getChartWeightBMR(), 
-                            chartTabs.getChartWeightCal(), chartTabs.getChartHeightBMR());
+        //    chartTabs = new ChartTabPane();
+        chartTabPane = new TabPane();
+        addTabsAndLineCharts();
+             person.addAllObserver(results, chartWeightBMR, chartWeightCal, 
+                                   chartHeightBMR);
 
         Button btnCalcul = makeButtonCalcul();
         Button btnClear = makeButtonClear();
@@ -67,17 +78,73 @@ public class BMRfx extends Application {
 
         root.setTop(menuBar);
         root.setLeft(dataResultBtnZone);
-        root.setCenter(chartTabs);
+        root.setCenter(chartTabPane);
 
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
+    private void addTabsAndLineCharts() {
+        makeLineCharts();
+        makeTabs();
+        chartTabPane.getTabs().addAll(tabWeightBMR, tabWeightCal, tabHeightBMR);
+    }
+
+    private void makeTabs() {
+        tabWeightBMR = new Tab("Weight (kg) Vs BMR");
+        tabWeightCal = new Tab("Weight (kg) Vs Calories");
+        tabHeightBMR = new Tab("Height (cm) Vs BMR");
+        addChartsToTabs();
+    }
+
+    private void addChartsToTabs() {
+        tabWeightBMR.setContent(chartWeightBMR);
+        tabWeightCal.setContent(chartWeightCal);
+        tabHeightBMR.setContent(chartHeightBMR);
+
+    }
+
+    private void makeLineCharts() {
+        initChartHeightBMR();
+        initChartWeightBMR();
+        initChartWeightCal();
+    }
+
+    private void initChartHeightBMR() {
+        NumberAxis xAxis = makeXAxis("Height (cm)");
+        NumberAxis yAxis = makeYAxis("BMR");
+        chartHeightBMR = new LineCharts("Height (cm) Vs BMR", xAxis, yAxis);
+    }
+
+    private void initChartWeightBMR() {
+        NumberAxis xAxis = makeXAxis("Weight (kg)");
+        NumberAxis yAxis = makeYAxis("BMR");
+        chartWeightBMR = new LineCharts("Weight (kg) Vs BMR", xAxis, yAxis);
+    }
+
+    private void initChartWeightCal() {
+        NumberAxis xAxis = makeXAxis("Weight (kg)");
+        NumberAxis yAxis = makeYAxis("Calories");
+        chartWeightCal = new LineCharts("Weight (kg) Vs Calories", xAxis, yAxis);
+    }
+
     private void setStage(Stage primaryStage) {
         primaryStage.setTitle("BMR Calculator");
         primaryStage.setMinWidth(400);
         primaryStage.setMinHeight(250);
+    }
+
+    private NumberAxis makeXAxis(String label) {
+        NumberAxis xAxis = new NumberAxis(0, 200, 25);
+        xAxis.setLabel(label);
+        return xAxis;
+    }
+
+    private NumberAxis makeYAxis(String label) {
+        NumberAxis yAxis = new NumberAxis(0, 3000, 250);
+        yAxis.setLabel(label);
+        return yAxis;
     }
 
     private HBox makeInfosBox() {
